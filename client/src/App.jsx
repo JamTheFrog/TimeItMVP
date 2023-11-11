1e5dd85dd4ec872b4cc461af239792b9108e0c19
eminmuftic <emin.muftic2004@gmail.com>
2023-11-11 19:13:11 +0100

    routing setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,37 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import LandingPage from "./pages/marketing/LandingPage";
+import Signup from "./pages/auth/Signup";
+import Signin from "./pages/auth/Signin";
+import Signout from "./pages/auth/Signout";
+import NotFound from "./pages/error/NotFound";
+
 function App() {
   return (
-    <h1 className="text-3xl bg-primary font-primaryFont uppercase">
-      gfdsfdsfs
-    </h1>
+    <Router>
+      {/* <MainNavigation /> */}
+      <div className="mt-[4rem] bg-white">
+        <Switch>
+          {/* MARKETING ROUTES */}
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          {/* AUTH ROUTES */}
+          <Route exact path="/auth/signup">
+            <Signup />
+          </Route>
+          <Route exact path="/auth/signin">
+            <Signin />
+          </Route>
+          <Route exact path="/auth/signout">
+            <Signout />
+          </Route>
+          {/* ERROR ROUTES */}
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
