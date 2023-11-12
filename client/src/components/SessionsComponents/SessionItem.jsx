671484efbc21cb4a8c7ec2c425d228a8ed850ba1
eminmuftic <emin.muftic2004@gmail.com>
2023-11-13 00:10:09 +0100

    detailed buttons

--- client/src/components/SessionsComponents/SessionItem.jsx
+++ client/src/components/SessionsComponents/SessionItem.jsx
@@ -2,34 +2,29 @@ import React from "react";
 import Card from "../../shared/UIElements/Card";
 import { useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import {RxArrowRight} from 'react-icons/rx'
 
 function SessionItem({ session }) {
   const location = useLocation();
   const currentUser = useSelector((state) => state.auth.currentUser);
   return (
-    <Card>
+    <Card className={"flex flex-col items-center justify-between"}>
       <h1 className="text-primary font-semibold font-primaryFont text-xl mb-2">
         {session.name}
       </h1>
       <p className="text-gray-600">{session.description}</p>
-      {/* <button
-        type="submit"
-        className="text-white bg-primary hover:bg-primaryDark focus:ring-4 focus:outline-none focus:ring-primaryLight font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 mt-4 text-center "
-      >
-        Start session
-      </button> */}
       <Link
-        to={
-          currentUser
-            ? `/sessions/${session.id}`
-            : "/auth/signin"
-        }
+        className="text-white mt-4  bg-primary hover:bg-primaryDark focus:ring-4 focus:outline-none focus:ring-primaryLight font-medium rounded-lg text-sm px-5 py-2.5  "
+        to={currentUser ? `/sessions/${session.id}` : "/auth/signin"}
       >
-        <span className="font-semibold text-primary">Start session</span>
+        <span className="font-semibold ">Start session</span>
       </Link>
       {location.pathname.includes("ownersessions") && (
-        <Link to={`/sessions/${session.id}/createtimeblock`}>
-          <span className="font-semibold text-primary">Create time blocks</span>
+        <Link
+          className="text-darkHard mt-4 flex items-center gap-2 focus:ring-4 focus:outline-none focus:ring-primaryLight font-medium rounded-lg text-sm px-5 py-2.5  "
+          to={`/sessions/${session.id}/createtimeblock`}
+        >
+          <span className="font-semibold ">Create time blocks</span> <RxArrowRight className="mt-0.5 text-lg"/>
         </Link>
       )}
     </Card>
