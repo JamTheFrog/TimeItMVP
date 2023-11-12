9b753f4d469a045c369d2cf4b455b81ba41dff50
eminmuftic <emin.muftic2004@gmail.com>
2023-11-12 14:59:27 +0100

    prepared sessions actions

---
+++ client/src/store/actions/sessions-actions.jsx
@@ -0,0 +1,128 @@
+import axios from "axios";
+import { keys } from "../../keys/keys";
+import { errorsActions } from "../slices/errors-slice";
+import { sessionsActions } from "../slices/sessions-slice";
+
+export const postSession = (sessionData, token, onSuccess) => {
+  return async (dispatch) => {
+    const sendRequest = async () => {
+      const response = await axios.post(
+        `${keys.apiUrl}/api/sessions`,
+        {
+          ...sessionData,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return response.data;
+    };
+    dispatch(errorsActions.setErrors([]));
+
+    try {
+      const createdSession = await sendRequest();
+      dispatch(errorsActions.setErrors([]));
+      onSuccess(createdSession);
+    } catch (error) {
+      const errors = error.response.data.errors;
+
+      dispatch(errorsActions.setErrors(errors));
+    }
+  };
+};
+
+export const getSessions = () => {
+  return async (dispatch) => {
+    const sendRequest = async () => {
+      const response = await axios.get(`${keys.apiUrl}/api/sessions`, {});
+      return response.data;
+    };
+    dispatch(errorsActions.setErrors([]));
+    try {
+      const sessionsData = await sendRequest();
+      dispatch(sessionsActions.setSessions(sessionsData));
+      dispatch(errorsActions.setErrors([]));
+    } catch (error) {
+      const errors = error.response.data.errors;
+      dispatch(errorsActions.setErrors(errors));
+    }
+  };
+};
+
+export const getOwnerSessions = (token) => {
+  return async (dispatch) => {
+    const sendRequest = async () => {
+      const response = await axios.get(
+        `${keys.apiUrl}/api/sessions/ownersessions`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return response.data;
+    };
+    dispatch(errorsActions.setErrors([]));
+    try {
+      const sessionsData = await sendRequest();
+      dispatch(sessionsActions.setSessions(sessionsData));
+      dispatch(errorsActions.setErrors([]));
+    } catch (error) {
+      const errors = error.response.data.errors;
+      dispatch(errorsActions.setErrors(errors));
+    }
+  };
+};
+
+export const getDetailSession = (sessionId, token) => {
+  return async (dispatch) => {
+    const sendRequest = async () => {
+      const response = await axios.get(
+        `${keys.apiUrl}/api/sessions/${sessionId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return response.data;
+    };
+    dispatch(errorsActions.setErrors([]));
+    try {
+      const detailSession = await sendRequest();
+      dispatch(sessionsActions.setDetailSession(detailSession));
+      dispatch(errorsActions.setErrors([]));
+    } catch (error) {
+      const errors = error.response.data.errors;
+      dispatch(errorsActions.setErrors(errors));
+    }
+  };
+};
+
+export const postTimeBlock = (timeBlockData, sessionId, token) => {
+  return async (dispatch) => {
+    const sendRequest = async () => {
+      const response = await axios.get(
+        `${keys.apiUrl}/api/sessions/${sessionId}/timeblocks`,
+        { ...timeBlockData },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return response.data;
+    };
+    dispatch(errorsActions.setErrors([]));
+    try {
+      const detailSession = await sendRequest();
+      dispatch(sessionsActions.setDetailSession(detailSession));
+      dispatch(errorsActions.setErrors([]));
+    } catch (error) {
+      const errors = error.response.data.errors;
+      dispatch(errorsActions.setErrors(errors));
+    }
+  };
+};
